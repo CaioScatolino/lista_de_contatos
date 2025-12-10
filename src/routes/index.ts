0bc296a29edc036efff92146487723b6547ae5f2
Caio <caio.scatolino@outlook.com>
2025-12-10 19:40:27 -0300

    feat: add basic contact list API with Express server and contact saving to JSON file.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,40 @@
 import express from "express";
-
+import { readFile, writeFile } from "fs/promises";
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
+const dataSource = "data/list.json";
+
+router.post("/contato", async (req, res) => {
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).json({ error: "Name is required" });
+  } else if (name.length < 2) {
+    return res
+      .status(400)
+      .json({ error: "Name must be at least 2 characters long" });
+  }
+
+  // TODO: Save contact in database
+  let list: string[] = [];
+
+  try {
+    const data = await readFile(dataSource, "utf-8");
+    list = JSON.parse(data);
+  } catch (err) {
+    console.log(err);
+  }
+
+  list.push(name);
+
+  try {
+    await writeFile(dataSource, JSON.stringify(list));
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({ error: "Failed to save contact" });
+  }
+
+  return res.status(201).json({ contato: name });
 });
 
 export default router;
